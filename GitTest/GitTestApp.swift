f7a3c8980ffeb44a0ba9a2cd822251bc142c6aee
Guilherme Gomes <guilhermerrg15@gmail.com>
2022-10-11 15:06:23 -0300

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Guilherme Gomes on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
